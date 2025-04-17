f60915a55133b983ca2706bc27687ff889c79398
Manav6938 <manav.rajput@benepik.com>
2025-04-17 17:05:49 +0530

    Add Files

--- src/main/java/com/UserService/Entity/Admin/AdminRegistration.java
+++ src/main/java/com/UserService/Entity/Admin/AdminRegistration.java
@@ -1,11 +1,11 @@
 package com.UserService.Entity.Admin;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
 
 /**
  * @author Manav Rajput Java Developer
@@ -18,9 +18,26 @@ import lombok.NoArgsConstructor;
 @Table(name = "tbl_adminRegistration")
 public class AdminRegistration {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "adminId", nullable = false)
     private Long adminId;
+
+    @Column(name = "firstName", nullable = false, length = 255)
     private String firstName;
+
+    @Column(name = "lastName", nullable = false, length = 255)
     private String lastName;
+
+    @Column(name = "contactNo", nullable = false, length = 50)
     private String contactNo;
 
+    @Column(name = "status", nullable = false, length = 10)
+    private boolean status;
+
+    @Column(name = "email", nullable = false, length = 255)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 255)
+    private String password;
 }
